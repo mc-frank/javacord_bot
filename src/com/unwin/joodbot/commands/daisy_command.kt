983edd827836756e3af2dfb1aa3b9e3c86f0bb9b
Sam Unwin <unwin100@gmail.com>
2016-06-06 14:44:13 +0100

    Execute, subreddit and other fixes
    - Execute command only allowed by members with IDs in the
    "execute_allowed" array in "api"
    - Subreddit now takes longer to fetch a post as the old method wasn't
    letting me reauthenticate for some reason
    - reddit_client gone - not needed anymore
    - Filter fixes
    - Incremented version to 4.2.0 #BlazeIt

--- src/com/unwin/joodbot/commands/daisy_command.kt
+++ src/com/unwin/joodbot/commands/daisy_command.kt
@@ -1,11 +1,12 @@
 package com.unwin.joodbot.commands
 
-import com.unwin.joodbot._reddit
-import com.unwin.joodbot._reddit_client
 import com.unwin.joodbot.json_reader
+import de.btobastian.javacord.entities.message.Message
 import de.btobastian.sdcf4j.Command
 import de.btobastian.sdcf4j.CommandExecutor
 import net.dean.jraw.RedditClient
+import net.dean.jraw.http.UserAgent
+import net.dean.jraw.http.oauth.Credentials
 
 /**
  * Created by unwin on 27-Mar-16.
@@ -13,7 +14,7 @@ import net.dean.jraw.RedditClient
 class daisy_command : CommandExecutor {
 
     @Command(aliases = arrayOf("daisy", "Daisy"), description = "Returns a random link from /r/daisyridley")
-    fun onCommand(command: String, args: Array<String>): String {
+    fun onCommand(command: String, args: Array<String>, message: Message): String {
 
         var j_reader = json_reader()
         j_reader.read_json_config()
@@ -21,13 +22,15 @@ class daisy_command : CommandExecutor {
             return "Reddit functions aren't enabled :("
         }
 
-        var link = "link-null"
+        message.getReceiver().type()
 
-        var redditClient = _reddit_client as RedditClient
-        if( !(redditClient.isAuthenticated) ) {
-            _reddit.authenticate()
-            redditClient = _reddit_client as RedditClient
-        }
+        val userAgent = UserAgent.of("discord-bot", "com.unwin.discordbot", "v3.0", "joodbot")
+        val redditClient = RedditClient(userAgent)
+        val credentials = Credentials.script(j_reader.r_username, j_reader.r_password, j_reader.r_client_id, j_reader.r_client_secret)
+        val auth_data = redditClient.oAuthHelper.easyAuth(credentials)
+        redditClient.authenticate(auth_data)
+
+        var link = "link-null"
 
         try {
             var post = redditClient.getRandomSubmission("DaisyRidley")
--- src/com/unwin/joodbot/commands/filter_command.kt
+++ src/com/unwin/joodbot/commands/filter_command.kt
@@ -13,7 +13,9 @@ class filter_command : CommandExecutor {
     @Command(aliases = arrayOf("filter"), description = "Filters text in the com.unwin.joodbot.log file")
     fun onCommand(command: String, args: Array<String>, message: Message): String {
         var word = ""
-        args.forEach { word += " $it" }
+        args.forEach { word += " ${it.toLowerCase()}" }
+        word = word.toLowerCase()
+        println("word == $word")
 
         var server_id = message.channelReceiver.server.id
         var count = log().filter(word, server_id)
